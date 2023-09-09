1651af1cc84816dbd3cdd84720b11ae3e66447e4
jonas-45 <jonaswullah@gmail.com>
2023-09-09 02:50:46 +0000

    Add greeting redux slice

---
+++ src/redux/greetings/greetingsSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getRandomGreeting = createAsyncThunk('greeting', async () => {
+  try{
+    const response = await fetch('localhost:3000/api/random_greeting');
+
+    if(!response.ok){
+      throw new Error('Could not get greeting');
+    }
+
+    const data = await response.json();
+    return data.greeting;
+  }catch(error){
+    throw new Error('Request failed');
+  }
+  
+})
+
+const initialState = {
+  message: ""
+}
+
+const GreetingsSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  extraReducers(builder){
+    builder.addCase(getRandomGreeting.fulfilled, (state,action) => {
+      return {
+        ...state,
+        message: action.payload
+      }
+    })
+  }
+
+})
+
+export default GreetingsSlice.reducer;
