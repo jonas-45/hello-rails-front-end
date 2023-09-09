23447a88464103dcc2218915732a614680058aa7
jonas-45 <jonaswullah@gmail.com>
2023-09-09 03:38:28 +0000

    Add greeting component

---
+++ src/components/greeting.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRandomGreeting } from "../redux/greetings/greetingsSlice";
+
+const Greeting = () => {
+  const theState = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log('state: ', theState)
+    dispatch(getRandomGreeting());
+  }, [dispatch]);
+
+  const handleRefresh = () => {
+    dispatch(getRandomGreeting());
+  }
+
+  return (
+    <div>
+      <h2>Random Greeting</h2>
+      <p className="greeting">{theState.message}</p>
+      <button type="button" onClick={handleRefresh}>Refresh</button>
+    </div>
+  )
+
+}
+
+export default Greeting;
